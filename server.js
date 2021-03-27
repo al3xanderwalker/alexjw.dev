6dc27919ef4111e4359332f21ba8d1c964170375
al3xanderwalker <48996881+al3xanderwalker@users.noreply.github.com>
2021-03-27 00:54:44 +0000

    Fix page not resolving

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 // Routing
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
 app.listen(8080, () => {
   console.log('listening on 8080');
 });
